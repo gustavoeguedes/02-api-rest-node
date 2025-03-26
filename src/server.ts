f01877464a152cccd91d3856876380dcbc8f2230
gustavoeguedes <gustavoestevesguedes12@gmail.com>
2025-03-26 08:39:58 -0300

    add database setup with Knex and SQLite; update server route to fetch schema

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'lehho world'
+app.get('/hello', async () => {
+  const test = await knex('sqlite_schema').select('*')
+  return test
 })
-
 app
   .listen({
     port: 3333,
